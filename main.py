7ec9a1e0424a1ddd2a064514e0bcebe43d695110
Dannilo Souza <dannilo.costa@dcomp.ufs.br>
2024-10-11 20:25:38 -0300

    Add tutorial

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import subprocess
 import sys
+import webbrowser
 
 if __name__=="__main__":
     subprocess.check_call([sys.executable, 'setup.py'])
+    webbrowser.open("http://127.0.0.1:5000/")
     subprocess.check_call([sys.executable, 'app.py'])
